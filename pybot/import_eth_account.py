af20429bcbb9861dba829a92da84e53a525b3a68
masato25 <leomasato25@gmail.com>
2023-06-26 13:41:05 +0800

    chore: change code of import eth account

--- pybot/import_eth_account.py
+++ pybot/import_eth_account.py
@@ -34,12 +34,25 @@ def import_eth_account(private_key='0x000000000000000000000000000000000000000000
     return json.loads(data)
 
 
+def get_current_node_url():
+    connection = init_connection()
+    request_url = f'https://{host}:{port}/network/status?chain=ethereum&network=arbitrum_one'
+    logging.info(f'request_url: {request_url}')
+    connection.request(method="GET", url=request_url, headers=request_headers)
+    # Print the HTTP response from the IOT service endpoint
+    response = connection.getresponse()
+    logging.info(f'get_current_url: {response.status} {response.reason}')
+    data = response.read()
+    logging.debug(f'data: {data}')
+    return json.loads(data)
+
+
 def main():
     input = None
     if len(sys.argv) > 1:
         input = sys.argv[1]
     if input is None or len(input) == 0:
-        logging.error('missing private key: ex: python import_eth_account.py import_eth_account')
+        logging.error('missing private key: ex: python import_eth_account.py eth_private_key')
         sys.exit(1)
     else:
         if input.startswith('0x'):
